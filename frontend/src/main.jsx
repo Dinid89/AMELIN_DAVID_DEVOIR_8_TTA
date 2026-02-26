c71d53ad194168a10f4a86f4842c6aecacaf573a
David A <david.amelin62@gmail.com>
2026-02-26 11:10:45 +0100

    Page d'acceuil - Titre + premiers contenus

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,47 +3,64 @@ import "./Home.scss"
 function Home() {
   return (
     <>
-  <h1>Comment trouver mon artisan ?</h1>
+    
+    <section className="welcome row text-justify">
+    <h1>Bienvenue sur notre plateforme</h1>
+    <p><strong>Trouve Ton Artisan</strong> est la vitrine digitale des savoir-faire d’Auvergne-Rhône-Alpes.</p>
+      <p>Ce site rassemble en un seul endroit tous les artisans de la région, soigneusement classés par catégories de métier : bâtiment, services, fabrication et alimentation.</p>
+      <p>En quelques clics, particuliers et professionnels peuvent découvrir des talents locaux, comparer les spécialités et trouver le bon expert près de chez eux.</p>
+      <p>Simple, clair et pensé pour valoriser le travail artisanal, <strong>Trouve Ton Artisan</strong> crée un pont direct entre les besoins du quotidien et les mains expertes qui font vivre notre territoire.</p>
+    </section>
 
-  <div className="row">
+    <section className="findArtisan">
+    <h2>Comment trouver mon artisan ?</h2>
+     
+    <div className="card-group row row-cols-1 g-3 row-cols-md-2 row-cols-lg-4 g-2">
+      
+      <div className="col">
+      <div className="card">
+        <h5 className="card-header">1 - Choisir la catégorie</h5>
+          <div className="card-body">
+            <p className="card-text">Choisissez la catégorie qui vous convient dans le menu en haut de la page.</p>
+          </div>
+      </div>
+      </div>
 
-  <div className="questionBox">
-    <div className="questionTitle">
-      <h3>Question 1</h3>
-    </div>
-    <div className="questionInside">
-      <p>Questions</p>
-    </div>
-  </div>
+      <div className="col">
+      <div className="card">
+        <h5 className="card-header">2 - Choisir un artisan</h5>
+          <div className="card-body">
+            <p className="card-text">Choissiez un artisan dans la liste proposée en cliquant sur le bouton "en savoir plus".</p>
+          </div>
+      </div>
+      </div>
 
-  <div className="questionBox">
-    <div className="questionTitle">
-      <h3>Question 2</h3>
-    </div>
-    <div className="questionInside">
-      <p>Questions</p>
-    </div>
-  </div>
+      <div className="col">
+      <div className="card">
+        < h5 className="card-header">3 - Le formulaire</h5>
+          <div className="card-body">
+            <p className="card-text">Envoyez un message à votre artisan via le formulaire de contact dans sa fiche.</p>
+          </div>
+      </div>
+      </div>
 
-  <div className="questionBox">
-    <div className="questionTitle">
-      <h3>Question 3</h3>
-    </div>
-    <div className="questionInside">
-      <p>Questions</p>
-    </div>
-  </div>
+      <div className="col">
+      <div className="card">
+        <h5 className="card-header">4 - Attendez votre réponse</h5>
+          <div className="card-body">
+            <p className="card-text">Une réponse de votre artisan vous sera apportée sous 48 heures par mail ou téléphone.</p>
+          </div>
+      </div>
+      </div>
 
-  <div className="questionBox">
-    <div className="questionTitle">
-      <h3>Question 4</h3>
-    </div>
-    <div className="questionInside">
-      <p>Questions</p>
-    </div>
-  </div>
+    </div> 
+    </section>
 
-  </div>
+    <section className="topArtisan">
+    <h2>Le top 3 du moment  </h2>
+    </section>
+   
+   
   </>
 
   )
