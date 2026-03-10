8bac51e67c6fdc4524b02dc9d4fc4121c334baf5
David A <david.amelin62@gmail.com>
2026-03-10 14:25:45 +0100

    deploiement vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Importation des pages
 import Home from './pages/Home/Home'
-import ArtisanList from './pages/artisanList/ArtisanList'
+import ArtisanList from './pages/ArtisanList/ArtisanList'
 import ArtisanDetail from './pages/ArtisanDetail/ArtisanDetail'
 import Legal from './pages/MentionLegale/MentionLegale'
 import NotFound from './pages/NotFound/NotFound'
